1295acde9e6269369845d69c06d9e56e9e18ff16
gravin <gravingu@163.com>
2021-04-05 23:33:31 +0800

    1

---
+++ Chapter03/chapter3-practise/main.js
@@ -0,0 +1,11 @@
+new Vue({
+    name: 'game',
+    el: '#app',
+    data: state,
+    template: `<div id="#app">
+{{worldRatio}}
+</div>`,
+    mounted() {
+        console.log(this.$data === state);
+    }
+})
